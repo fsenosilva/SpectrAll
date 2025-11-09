cffea0083635997fc1b4ec6a3e2ea119d5ea73f7
fseno <fsenosilva@gmail.com>
2025-11-09 16:54:43 +0000

    Initial

---
+++ js/mail.js
@@ -0,0 +1,24 @@
+function sendEmail(event){
+
+    const email = ['su','pp','ort','@','spe','ctr','all','.eu'];
+    const to = encodeURIComponent(email.join(''));
+    const name = encodeURIComponent(event.name.value);
+    const body = encodeURIComponent(event.message.value);
+    const subject = encodeURIComponent(`<INFO> ${name}`);
+
+    let mailto = `mailto:${to}?subject=${subject}&body=${body}`;
+
+    window.location.href = mailto;
+    event.preventDefault();
+    
+
+}
+
+function copyEmail() {
+  const emailInput = document.getElementById("emailAddress");
+  const message = document.getElementById("copyMessage");
+
+  navigator.clipboard.writeText(emailInput.value);
+  message.classList.remove("hidden");
+  setTimeout(() => message.classList.add("hidden"), 2000);
+}
---
+++ js/styles.js
@@ -0,0 +1,21 @@
+    tailwind.config = {
+      theme: {
+        extend: {
+          colors: {
+            brand: '#8ddcab',
+            brand2: '#c0f0b0',
+            dark: '#0f172a',
+            headings: '#70b088',
+            ntext: '#595959'
+
+          },
+           backgroundImage: {
+             'grid': 'linear-gradient(rgba(0,0,0,0.05) 1px, transparent 1px), linear-gradient(to right, rgba(0,0,0,0.05) 1px, transparent 1px)',
+          },
+           backgroundSize: {
+              'grid': '20px 20px',
+            }
+        },
+      },
+    };
+
